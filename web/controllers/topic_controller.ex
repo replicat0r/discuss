7f70e72009151eeab468479da444d8932b22fce9
Tyler <tyler@nanoleaf.me>
2018-03-31 20:12:44 -0400

    added delete action and simplified routes

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -51,4 +51,13 @@ defmodule Discuss.TopicController do
     end
 
   end
+
+  def delete(conn, %{"id"=> id}) do
+    Repo.get!(Topic, id)
+    |>Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
 end
